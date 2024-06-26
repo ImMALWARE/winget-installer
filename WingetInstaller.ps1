8810a18e4a327702a1d4c423ef37fccb52fd88f3
MALWARE <53017160+ImMALWARE@users.noreply.github.com>
2024-06-26 18:42:02 +0300

    ~

--- WingetInstaller.ps1
+++ WingetInstaller.ps1
@@ -2,7 +2,7 @@ $wc = New-Object net.webclient
 cd $env:temp
 $wc.DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$env:temp\VCLibs.appx")
 Add-AppPackage -Path VCLibs.appx -Verbose
-Write-Host "Ждите, это долго, скачивается файл 200 МБ"
+Write-Host "200 MB file is downloading, this will take some minutes."
 $wc.DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.8.1762/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle', "$env:temp\DesktopAppInstaller.msixbundle")
 $wc.DownloadFile('https://raw.githubusercontent.com/ImMALWARE/winget-installer/main/Microsoft.UI.Xaml.2.8.appx', "$env:temp\UIXaml.appx")
 Add-AppxPackage -Path UIXaml.appx -Verbose
