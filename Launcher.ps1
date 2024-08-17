9bbce0732136e494e5928767655fa704ef279adb
MALWARE <53017160+ImMALWARE@users.noreply.github.com>
2024-08-17 17:49:57 +0300

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,4 +1,4 @@
 $host.UI.RawUI.WindowTitle = "Starting winget-installer..."
 Add-Type -Name Window -Namespace Console -MemberDefinition '[DllImport("Kernel32.dll")]public static extern IntPtr GetConsoleWindow();[DllImport("user32.dll")]public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 [void][Console.Window]::ShowWindow([Console.Window]::GetConsoleWindow(), 0)
-Start-Process powershell -ArgumentList "(Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/ImMALWARE/winget-installer/WingetInstaller.ps1).Content | Invoke-Expression" -Verb RunAs
+Start-Process powershell -ArgumentList "(Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/ImMALWARE/winget-installer/main/WingetInstaller.ps1).Content | Invoke-Expression" -Verb RunAs
