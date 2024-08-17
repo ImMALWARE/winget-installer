46c111b35d325037d12691aba8d929cf781bdcc5
MALWARE <53017160+ImMALWARE@users.noreply.github.com>
2024-08-17 17:28:01 +0300

    License path for DesktopAppInstaller

--- WingetInstaller.ps1
+++ WingetInstaller.ps1
@@ -5,7 +5,8 @@ Add-AppPackage -Path VCLibs.appx -Verbose
 Write-Host "200 MB file is downloading, this will take some minutes."
 $wc.DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.8.1762/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle', "$env:temp\DesktopAppInstaller.msixbundle")
 $wc.DownloadFile('https://raw.githubusercontent.com/ImMALWARE/winget-installer/main/Microsoft.UI.Xaml.2.8.appx', "$env:temp\UIXaml.appx")
+$wc.DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.8.1762/fc2c4f481bae4326b6104907449dad99_License1.xml', "$env:temp\License1.xml")
 Add-AppxPackage -Path UIXaml.appx -Verbose
-Add-AppxPackage -Path DesktopAppInstaller.msixbundle -Verbose
+Add-AppxProvisionedPackage -Online -PackagePath DesktopAppInstaller.msixbundle -LicensePath License1.xml -Verbose
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [System.Windows.Forms.MessageBox]::Show('Winget is installed!')
